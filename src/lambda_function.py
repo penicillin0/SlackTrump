35c2e1855eec9b6174a6268fe966fb0593a4af63
penicillin0 <ayahitonakamura@gmail.com>
2020-06-30 20:11:43 +0900

    can check joker num

--- src/lambda_function.py
+++ src/lambda_function.py
@@ -5,10 +5,9 @@ import random
 import boto3
 import datetime
 
-
 def lambda_handler(event, context):
 
-    ####################
+    ########################
     ### ここから各種検証 ###
 
     # リトライイベントなら終了
@@ -29,21 +28,24 @@ def lambda_handler(event, context):
         userID = body['event']['user']
         userName = os.environ[userID]
 
-    text = body['event']['text']
 
+    text = body['event']['text']
+    
     # 有効なテキストが入っていないなら終了
     if not checkValidText(text):
         return {'statusCode': 200, 'body': json.dumps('valid text')}
-
-    ### ここまで各種検証 ###
-    ####################
+    
+    ### ここまで各種検証 ###    
+    ########################
+    
 
     # dynamodbのアカウントとか
     client = boto3.client('dynamodb')
     d_today = str(datetime.date.today()).replace('-', '')
     primary_key = {"date": {"S": d_today}}
     item = client.get_item(TableName='TrumpHistory', Key=primary_key)
-
+    
+    
     text = body['event']['text']
     # 削除なら削除して終了
     if "delete" in text:
@@ -54,61 +56,80 @@ def lambda_handler(event, context):
         trumpinfo = item['Item']['trump']["L"]
     else:
         trumpinfo = []
-
+        
+        
     # 重複を削除
     trumpinfo_wo_daburi, drawn_trump_set = resolveOverlap(trumpinfo)
 
     # トランプ全部
     all_trump = createAllTrump()
-
+    
     # トランプを引く
     all_trump_set = set(all_trump)
     able_draw_list = list(all_trump_set - drawn_trump_set)
     draw_trump = random.choice(able_draw_list)
-
+    
+    # jokerの枚数をチェック
+    joker_num_nokori = get_joker_num(able_draw_list)
+    if draw_trump == 'j1' or draw_trump == 'j2':
+        joker_num_nokori -= 1
+        
+    
+    
+    
     # 引いたトランプを加える
     trumpinfo_wo_daburi.append({'S': draw_trump})
     drawnTrumpNum = len(trumpinfo_wo_daburi)
-    remainTrumpNum = 52 - len(trumpinfo_wo_daburi)
+    remainTrumpNum = 54 - len(trumpinfo_wo_daburi)
 
+    
     append_item = {
         "date": {
             "S": d_today
         },
-        "trump": {
+        "trump" : {
             "L": trumpinfo_wo_daburi
         }
     }
-
+    
     client.put_item(TableName='TrumpHistory', Item=append_item)
-
+    
     if 'トランプ' in text:
-        rep = draw_trump.replace('d', '♦').replace('h', '♥').replace(
-            'k', '♣').replace('s', '♠').replace('j', ':black_joker:')
-
+        rep = draw_trump.replace('d','♦').replace('h','♥').replace('k','♣').replace('s','♠').replace('j', ':black_joker:')
+        
         post_message_to_channel(
-            "ーーーーー\n" +
-            "{:13}".format('|'+rep)+'|\n' +
-            "{:8}".format('|'+userName+'さん')+'|'+'\n' +
+            "ーーーーー\n"+
+            "{:13}".format('|'+rep)+'|\n'+
+            "{:8}".format('|'+userName+'さん')+'|'+'\n'+
             "{:11}".format('|')+rep+'|\n' +
             "ーーーーー\n\n"
-            'カードの残り枚数:'+str(remainTrumpNum)+'\n\n' +
-            '引かれたカードの枚数:'+str(drawnTrumpNum)+'\n\n')
-        # 'DB確認用(後で消す)'+'\n'+
-        # '------------------'+'\n'+
-        # json.dumps(append_item))
+            ':トランプ:の残り枚数:'+str(remainTrumpNum)+'\n\n\n\n'+
+            '引かれた:トランプ:の枚数:'+str(drawnTrumpNum)+'\n\n'+
+            '引かれた:black_joker:の枚数:'+str(2 - joker_num_nokori)+'\n\n'
+            )
+            # 'DB確認用(後で消す)'+'\n'+
+            # '------------------'+'\n'+
+            # json.dumps(append_item))
+    
 
     return {'statusCode': 200, 'body': json.dumps('ok')}
 
 
+def get_joker_num(trump_list):
+    joker_num = 0
+    for trump in trump_list:
+        if 'j' in trump:
+            joker_num += 1
+    return joker_num
+        
+    
 def post_message_to_channel(message):
     url = os.environ['SLACK_WEBHOOK_URL']
     data = {
         "text": message,
     }
 
-    req = urllib.request.Request(url, data=json.dumps(
-        data).encode("utf-8"), method="POST")
+    req = urllib.request.Request(url, data=json.dumps(data).encode("utf-8"), method="POST")
     urllib.request.urlopen(req)
 
 
@@ -117,13 +138,12 @@ def format_DB(d_today, client):
         "date": {
             "S": d_today
         },
-        "trump": {
+        "trump" : {
             "L": []
         }
     }
     client.put_item(TableName='TrumpHistory', Item=clear_item)
     post_message_to_channel(str(d_today)+"のDBをクリアしました")
-
 
 def reserchTimeoutOrNot(event):
     if 'x-slack-retry-reason' in event['headers']:
@@ -132,26 +152,22 @@ def reserchTimeoutOrNot(event):
             # post_message_to_channel("以下の理由で要求を棄却しました"+event['headers']['x-slack-retry-reason'])
             return True
     return False
-
-
+    
 def createAllTrump():
     all_trump = []
-    for i in range(1, 14):
+    for i in range(1,14):
         for kigou in ['d', 's', 'h', 'k']:
             all_trump.append(kigou + str(i))
     all_trump.append('j1')
     all_trump.append('j2')
     return all_trump
-
-
+    
 def checkValidText(text):
-    boolean = False
     validTextList = ['トランプ', 'delete']
     for validText in validTextList:
         if validText in text:
             return True
     return False
-
 
 def resolveOverlap(trumpinfo):
     # trumpinfo -> trumpinfo_wo_dauri
@@ -163,3 +179,4 @@ def resolveOverlap(trumpinfo):
             trumpinfo_wo_daburi.append(trump)
     drawn_trump_set = set(drawn_trump_list)
     return trumpinfo_wo_daburi, drawn_trump_set
+    
